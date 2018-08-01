81dc3e6fdb70d6d49ad43e96f4398265f547acaf
Christopher Edwards <Christopher.Edwards@krminc.com>
2018-08-01 12:25:40 -0400

    Initial shot at getting modal windows for regions

--- www/autoptimize.js
+++ www/autoptimize.js
@@ -761,3 +761,27 @@ jQuery(document).ready(function($) {
                                     f.height = g } if ("link" === d.message)
                                     if (h = b.createElement("a"), i = b.createElement("a"), h.href = f.getAttribute("src"), i.href = d.value, i.host === h.host)
                                         if (b.activeElement === f) a.top.location.href = d.value } else; } }, d) a.addEventListener("message", a.wp.receiveEmbedMessage, !1), b.addEventListener("DOMContentLoaded", c, !1), a.addEventListener("load", c, !1) }(window, document);
+// from: https://www.w3schools.com/howto/howto_css_modals.asp
+// Get the modal
+var modal = document.getElementById('regionDetails');
+var modalContent = document.getElementById('modal-content');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+regionDetails = function(region) {
+    modal.style.display = "block";
+    modalContent.innerHTML ="<span class=\"close\">&times;</span><p>Region Details</p><p>Region: " + region + "</p>\n<p>Segment: " + region + "</p>";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
